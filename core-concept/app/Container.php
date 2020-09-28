431adb2b2707543802748a1f6191ed66ed0df02e
bappi2097 <bappi35-2097@diu.edu.bd>
2020-09-28 13:31:57 +0600

    service container added

---
+++ core-concept/app/Container.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class Container
+{
+    protected $bindings = [];
+    public function bind($key, $value)
+    {
+        $this->bindings[$key] = $value;
+    }
+    public function resolve($key)
+    {
+        if (isset($this->bindings[$key])) {
+            return call_user_func($this->bindings[$key]);
+        }
+    }
+}
--- core-concept/routes/web.php
+++ core-concept/routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    $container = new \App\Container();
+    $container->bind('example', function () {
+        return new \App\Example();
+    });
+
+    $example = $container->resolve('example');
+    $example->go();
+    ddd($example);
 });
