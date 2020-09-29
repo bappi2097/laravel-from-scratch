21eb62f9cee67aeb1e8ec7acb014a72bb93efd60
bappi2097 <bappi35-2097@diu.edu.bd>
2020-09-29 19:55:02 +0600

    sms notification added

--- notification/app/Models/User.php
+++ notification/app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    /**
+     * Route notifications for the Nexmo channel.
+     *
+     * @param  \Illuminate\Notifications\Notification  $notification
+     * @return string
+     */
+    public function routeNotificationForNexmo($notification)
+    {
+        return "8801726257333";
+    }
 }
--- notification/app/Notifications/PaymentReceived.php
+++ notification/app/Notifications/PaymentReceived.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\NexmoMessage;
 
 class PaymentReceived extends Notification
 {
@@ -30,7 +31,7 @@ class PaymentReceived extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail', 'database'];
+        return ['mail', 'database', 'nexmo'];
     }
 
     /**
@@ -47,6 +48,17 @@ class PaymentReceived extends Notification
             ->line('Lorem ipsum sit amet.')
             ->action('Sign Up', url('/'))
             ->line('Thanks');
+    }
+    /**
+     * Get the Nexmo / SMS representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return NexmoMessage
+     */
+    public function toNexmo($notifiable)
+    {
+        return (new NexmoMessage)
+            ->content('Your payment has been received');
     }
 
     /**
